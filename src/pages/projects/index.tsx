7e6ac524a05fb2a7fa2bdc8343a7619a03b41326
Bernardo Pereira <bernardopereiraoliveira258@gmail.com>
2024-08-05 22:52:00 -0300

    feat: implementado busca por repositórios

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,20 +1,28 @@
 import { ReactElement, useEffect } from 'react'
 
+import { RepositoryGithub, UserGithub } from '@/@types/models/github.model'
 import { UnderConstruction } from '@/components/UnderConstruction'
 import { Default } from '@/layouts/Default'
-import { octokit } from '@/services/octokit'
+import { getRepositoriesByUser, getUserAuthenticated } from '@/services/octokit'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
-const Projects = () => {
+interface ProjectsProps {
+  environment: string
+  bernardoSemiOficial: UserGithub
+  repositories: RepositoryGithub
+}
+
+const Projects = ({ bernardoSemiOficial, repositories }: ProjectsProps) => {
   const getUsersAuthenticated = async () => {
-    const {
-      data: { login },
-    } = await octokit.rest.users.getAuthenticated()
-    console.log('Hello, %s', login)
+    const username = 'BernardoSemiOficial'
+    const data = await getUserAuthenticated()
+    const repositories = await getRepositoriesByUser(username)
+    console.log('data', data, repositories)
   }
 
   useEffect(() => {
-    getUsersAuthenticated()
+    console.log(bernardoSemiOficial, repositories)
+    // getUsersAuthenticated()
   }, [])
 
   return <UnderConstruction />
@@ -25,11 +33,18 @@ interface GetStaticProps {
 }
 
 export async function getStaticProps({ locale }: GetStaticProps) {
+  const username = 'BernardoSemiOficial'
+  const data = await getUserAuthenticated()
+  const repositories = await getRepositoriesByUser(username)
+
   return {
     props: {
       environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
+      bernardoSemiOficial: data,
+      repositories,
       ...(await serverSideTranslations(locale ?? 'pt', ['common'])),
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
 
--- src/services/octokit.ts
+++ src/services/octokit.ts
@@ -1,5 +1,25 @@
+import { restEndpointMethods } from '@octokit/plugin-rest-endpoint-methods'
 import { Octokit } from 'octokit'
 
-export const octokit = new Octokit({
+const MyOctokit = Octokit.plugin(restEndpointMethods)
+
+const octokit = new MyOctokit({
   auth: process.env.NEXT_PUBLIC_GITHUB_PERSONAL_ACCESS_TOKEN,
 })
+
+const headers = {
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28',
+  },
+}
+
+export const getUserAuthenticated = async () => {
+  return await octokit.rest.users.getAuthenticated()
+}
+
+export const getRepositoriesByUser = async (username: string) => {
+  return await octokit.request('GET /users/{username}/repos', {
+    username,
+    ...headers,
+  })
+}
